4122ff9e2cc18c975ede968b4a3f990c5cad1020
Moses Kisya <118076277+kishea-dev@users.noreply.github.com>
2023-02-24 02:08:26 +0300

    lesscode

--- getline.c
+++ getline.c
@@ -9,30 +9,32 @@
 ssize_t my_getline(char **lineptr, size_t *n, FILE *stream)
 {
 		if (!lineptr || !n || !stream)
-				return (-1);
+			return (-1);
 
 		size_t i = 0, max = *n;
 		int c;
 
-		while ((c = fgetc(stream)) != EOF) {
-				if (i == max - 1) {
-						max *= 2;
-						*lineptr = realloc(*lineptr, max);
-						if (!*lineptr)
-								return (-1);
-						*n = max;
-				}
+		while ((c = fgetc(stream)) != EOF)
+		{
+			if (i == max - 1)
+			{
+					max *= 2;
+					*lineptr = realloc(*lineptr, max);
+					if (!*lineptr)
+						return (-1);
+					*n = max;
+			}
 
-				(*lineptr)[i++] = c;
+			(*lineptr)[i++] = c;
 
-				if (c == '\n')
-						break;				
+			if (c == '\n')
+				break;
 		}
 
 		(*lineptr)[i] = '\0';
 
 		if (i == 0 && c == EOF)
-				return (-1);
+			return (-1);
 
 		return (i);
 }
