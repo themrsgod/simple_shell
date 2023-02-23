b95e7a0a6408029901b59519793aae0bd4fb8f3e
Moses Kisya <118076277+kishea-dev@users.noreply.github.com>
2023-02-23 03:25:06 +0300

    spaces to tabs

--- shell_get_cmd.c
+++ shell_get_cmd.c
@@ -6,23 +6,23 @@
  */
 char *get_line(void)
 {
-  char *buf = NULL;
-  size_t bufsize = 0;
-  int test;
+	char *buf = NULL;
+	size_t bufsize = 0;
+	int test;
 
-  test = getline(&buf, &bufsize, stdin);
-  buffers3(NULL, buf);
+	test = getline(&buf, &bufsize, stdin);
+	buffers3(NULL, buf);
 
-  if (test == EOF)
-    {
-      buffers1(NULL, NULL);
-      buffers2(NULL, NULL);
-      buffers3(NULL, NULL);
-      buffers4(NULL, NULL);
-      buffers5(NULL);
-      _exit(0);
-    }
-  return (buf);
+	if (test == EOF)
+		{
+			buffers1(NULL, NULL);
+			buffers2(NULL, NULL);
+			buffers3(NULL, NULL);
+			buffers4(NULL, NULL);
+			buffers5(NULL);
+			_exit(0);
+		}
+	return (buf);
 }
 
 /**
@@ -32,30 +32,30 @@ char *get_line(void)
  */
 char **split_line(char *line)
 {
-  char *dup_buf;
-  char *token;
-  char *toks;
-  char **tok;
-  int i = 1;
+	char *dup_buf;
+	char *token;
+	char *toks;
+	char **tok;
+	int i = 1;
 
-  dup_buf = _strdup(line);
-  token = strtok(line, DELIM);
-  while (token)
-    {
-      token = strtok(NULL, DELIM);
-      i++;
-    }
-  tok = malloc(4096);
-  buffers4(tok, NULL);
+	dup_buf = _strdup(line);
+	token = strtok(line, DELIM);
+	while (token)
+		{
+			token = strtok(NULL, DELIM);
+			i++;
+		}
+	tok = malloc(4096);
+	buffers4(tok, NULL);
 
-  toks = strtok(dup_buf, DELIM);
-  i = 0;
-  while (toks)
-    {
-      tok[i] = toks;
-      toks = strtok(NULL, DELIM);
-      i++;
-    }
-  tok[i] = '\0';
-  return (tok);
+	toks = strtok(dup_buf, DELIM);
+	i = 0;
+	while (toks)
+		{
+			tok[i] = toks;
+			toks = strtok(NULL, DELIM);
+			i++;
+		}
+	tok[i] = '\0';
+	return (tok);
 }
