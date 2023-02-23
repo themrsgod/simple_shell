ef0203d89dccb17c547a93fe4fda07a9005e0c79
moses kisya <kishea.dev@gmail.com>
2023-02-23 14:53:03 -0800

    get line

--- getline.c
+++ getline.c
@@ -1,45 +1,56 @@
-#include "main.h"
-
+#include <stdio.h>
+#include <stdlib.h>
 /**
 * getline - get line custom function
 * @lineptr: line pointer
 * @n: size of input
 * @stream: file input stream
 */
-#include <stdio.h>
-#include <stdlib.h>
 
-ssize_t getline(char **lineptr, size_t *n, FILE *stream)
+size_t my_getline(char **lineptr, size_t *n, FILE *stream)
 {
-	if (!lineptr || !n || !stream)
-		return (-1);
+  size_t bsz, i;
+  char *buf;
+  int c;
 
-	size_t bsz = *n, i = 0;
-	char *buf = *lineptr ? *lineptr : malloc(bsz = 64);
-	if (!buf)
-		return (-1);
-		
-	int c;
-	
-	while ((c = getc(stream)) != EOF)
-	{
-		if (i >= bsz - 1)
-		{
-			buf = realloc(buf, bsz *= 2);
-			if (!buf)
-				return (-1);
-		}
-		buf[i++] = c;
-		if (c == '\n')
-			break;
-	}
+  if (!lineptr || !n || !stream) {
+    return (-1);
+  }
 
-	buf[i] = '\0';
-	*lineptr = buf;
-	*n = bsz;
+  bsz = *n;
+  i = 0;
+  buf = *lineptr ? *lineptr : malloc(bsz = 64);
 
-	if (i == 0 && c == EOF)
-		return (-1);
+  if (!buf) {
+    return (-1);
+  }
 
-	return (i);
+  while ((c = getc(stream)) != EOF) {
+    if (i >= bsz - 1) {
+      char *new_buf = realloc(buf, bsz *= 2);
+
+      if (!new_buf) {
+	free(buf);
+	return (-1);
+      }
+
+      buf = new_buf;
+    }
+
+    buf[i++] = c;
+
+    if (c == '\n') {
+      break;
+    }
+  }
+
+  buf[i] = '\0';
+  *lineptr = buf;
+  *n = bsz;
+
+  if (i == 0 && c == EOF) {
+    return (-1);
+  }
+
+  return (i);
 }
