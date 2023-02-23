a76068e183cf84151b9f89528e48e30ba7085144
Moses Kisya <118076277+kishea-dev@users.noreply.github.com>
2023-02-24 02:22:31 +0300

    few issues

--- getline.c
+++ getline.c
@@ -10,33 +10,33 @@
 */
 ssize_t my_getline(char **lineptr, size_t *n, FILE *stream)
 {
-		size_t i, max;
-		int c;
+	size_t i, max;
+	int c;
 
-		if (!lineptr || !n || !stream)
-			return (-1);
-		i = 0;
-		max = *n;
+	if (!lineptr || !n || !stream)
+		return (-1);
+	i = 0;
+	max = *n;
 
-		while ((c = fgetc(stream)) != EOF)
+	while ((c = fgetc(stream)) != EOF)
+	{
+		if (i == max - 1)
 		{
-			if (i == max - 1)
-			{
-					max *= 2;
-					char *new_ptr = realloc(*lineptr, max);
-			
-					if (!new_ptr)
-							return (-1);
-					*lineptr = new_ptr;
-					*n = max;
-			}
-			(*lineptr)[i++] = c;
-			if (c == '\n')
-					break;
+				max *= 2;
+				char *new_ptr = realloc(*lineptr, max);
+		
+				if (!new_ptr)
+						return (-1);
+				*lineptr = new_ptr;
+				*n = max;
 		}
-		(*lineptr)[i] = '\0';
+		(*lineptr)[i++] = c;
+		if (c == '\n')
+				break;
+	}
+	(*lineptr)[i] = '\0';
 
-		if (i == 0 && c == EOF)
-			return (-1);
-		return (i);
+	if (i == 0 && c == EOF)
+		return (-1);
+	return (i);
 }
