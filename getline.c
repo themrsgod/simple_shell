f9a852102c312055bad95a352cd52b338cc79b51
Moses Kisya <118076277+kishea-dev@users.noreply.github.com>
2023-05-16 17:12:27 +0300

    get line

--- getline.c
+++ getline.c
@@ -10,33 +10,34 @@
 */
 ssize_t my_getline(char **lineptr, size_t *n, FILE *stream)
 {
-	size_t i, max;
-	int c;
+    size_t i, max;
+    int c;
+    char *new_ptr; // Declare new_ptr here
 
-	if (!lineptr || !n || !stream)
-		return (-1);
-	i = 0;
-	max = *n;
+    if (!lineptr || !n || !stream)
+        return (-1);
+    i = 0;
+    max = *n;
 
-	while ((c = fgetc(stream)) != EOF)
-	{
-		if (i == max - 1)
-		{
-				max *= 2;
-				char *new_ptr = realloc(*lineptr, max);
-		
-				if (!new_ptr)
-						return (-1);
-				*lineptr = new_ptr;
-				*n = max;
-		}
-		(*lineptr)[i++] = c;
-		if (c == '\n')
-				break;
-	}
-	(*lineptr)[i] = '\0';
+    while ((c = fgetc(stream)) != EOF)
+    {
+        if (i == max - 1)
+        {
+            max *= 2;
+            new_ptr = realloc(*lineptr, max); // Assign realloc result to new_ptr
 
-	if (i == 0 && c == EOF)
-		return (-1);
-	return (i);
+            if (!new_ptr)
+                return (-1);
+            *lineptr = new_ptr;
+            *n = max;
+        }
+        (*lineptr)[i++] = c;
+        if (c == '\n')
+            break;
+    }
+    (*lineptr)[i] = '\0';
+
+    if (i == 0 && c == EOF)
+        return (-1);
+    return (i);
 }
